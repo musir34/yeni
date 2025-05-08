59677c20ca12a56cbd303de91aa0ea53adce6043
Musir <musir@example.com>
2025-05-08 19:48:32 +0000

    Güncelleme: 2025-05-08 19:48:32

--- auto_push.sh
+++ auto_push.sh
@@ -3,3 +3,6 @@
 git add .
 git commit -m "Otomatik güncelleme: $(date +'%Y-%m-%d %H:%M:%S')" || echo "Değişiklik yok, commit atlanıyor"
 git push origin main || echo "Push başarısız!"
+
+#shell de güncelle
+#git config user.name "Musir" && git config user.email "musir@example.com" && git add . && git commit -m "Güncelleme: $(date +'%Y-%m-%d %H:%M:%S')" && git push
