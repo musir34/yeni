3fda8b760a3ff210e50a8d25bfd823d5eaef9401
apdurrahmankuli <36719948-apdurrahmankuli@users.noreply.replit.com>
2025-05-25 18:46:11 +0000

    Restored to 'a855af864b003beedcec16091a1403ff130176bc'
    Replit-Restored-To: a855af864b003beedcec16091a1403ff130176bc

--- setup_db.py
+++
@@ -1,9 +0,0 @@
-from app import app, db
-from models import CustomerQuestion
-
-if __name__ == "__main__":
-    with app.app_context():
-        # Eksik tabloları oluştur
-        print("Veritabanı tabloları oluşturuluyor...")
-        db.create_all()
-        print("Veritabanı tabloları başarıyla oluşturuldu!")
